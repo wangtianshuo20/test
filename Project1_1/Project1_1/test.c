d832d4f28de01578d44742845749d36148043ed6
wangtianshuo <1712424657@qq.com>
2020-12-26 14:35:22 +0800

    提交代码

---
+++ Project1_1/Project1_1/test.c
@@ -0,0 +1,18 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+#include<stdio.h>
+void print_test(int n) {
+	int i = 0;
+	for (i = 1; i <= n; i++) {
+		int j = 0;
+		for (j = 1; j <= i; j++) {
+			printf("%d*%d=%-3d", i, j, i * j);
+
+		}
+	}
+}
+int main() {
+	int n = 0;
+	scanf("%d", &n);
+	print_test(n);
+	return 0;
+}
